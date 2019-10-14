4c268a0c2414e6cbabcc10f169ceba4dc1546886
ArnoBoel <arno-boel@hotmail.be>
2019-10-14 20:50:06 +0200

    Project aanmaken met sdk 2.2

--- MultiMedAPI/MultiMedAPI/Program.cs
+++ MultiMedAPI/MultiMedAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MultiMedAPI
+namespace MultimedAPI
 {
     public class Program
     {
