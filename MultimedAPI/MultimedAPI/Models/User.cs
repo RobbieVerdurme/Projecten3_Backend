5103adfa3c91f183adb4651a9c8988392b9ced61
ArnoBoel <arno-boel@hotmail.be>
2019-10-14 20:55:58 +0200

    Add User model

---
+++ MultimedAPI/MultimedAPI/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultimedAPI.Models
+{
+    public class User
+    {
+
+        #region Properties
+
+        public string FirstName { get; set; }
+
+        public string FamilyName { get; set; }
+
+
+        #endregion
+
+        #region Constructors
+
+        public User()
+        {
+        }
+
+        public User(string firstName, string familyName)
+        {
+            FirstName = firstName;
+            FamilyName = familyName;
+        }
+
+
+
+        #endregion
+    }
+}
