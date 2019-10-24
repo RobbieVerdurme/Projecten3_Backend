041191857b73df922c1e4005807fdea0da2c873d
Shawn <Shawn@LAPTOP-5JTMJ9BR>
2019-10-25 00:19:53 +0200

    in comment voor mail te sturen

--- MultimedAPI/MultimedAPI/Controllers/UserController.cs
+++ MultimedAPI/MultimedAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -8,7 +9,9 @@ using Microsoft.AspNetCore.Mvc;
 using MultimedAPI.DTOs;
 using MultimedAPI.Models;
 using MultimedAPI.Models.IRepositories;
-
+using MailKit.Net.Smtp;
+using MimeKit;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
 
 namespace MultimedAPI.Controllers
 {
@@ -64,7 +67,29 @@ namespace MultimedAPI.Controllers
               .Select(s => s[random.Next(s.Length)]).ToArray());
             await _userManager.CreateAsync(user, password);
             _userRepository.SaveChanges();
+            #region voorEmail
+            //MimeMessage message = new MimeMessage();
 
+            //MailboxAddress from = new MailboxAddress("Admin",
+            //"shawn.vanranst@gmail.com");
+            //message.From.Add(from);
+
+            //MailboxAddress to = new MailboxAddress(userDTO.FirstName,
+            //userDTO.Email);
+            //message.To.Add(to);
+
+            //message.Subject = "uw wachtwoord voor multimed";
+            //BodyBuilder bodyBuilder = new BodyBuilder();
+            //bodyBuilder.HtmlBody = "<h1>hier is uw persoonlijk wachtwoord</h1>";
+            //bodyBuilder.TextBody = password;
+            //message.Body = bodyBuilder.ToMessageBody();
+            //SmtpClient client = new SmtpClient();
+            //client.Connect("smtp.live.com", 587, true);
+            //client.Authenticate("", "");
+            //client.Send(message);
+            //client.Disconnect(true);
+            //client.Dispose();
+            #endregion 
             return CreatedAtAction(nameof(AddUserAsync), new { id = userToCreate.UserId }, userToCreate);
         }
         #endregion
