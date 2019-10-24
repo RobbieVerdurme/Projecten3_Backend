c92619ca4eb2db3fc1f8042f68baf302e934003a
Shawn <Shawn@LAPTOP-5JTMJ9BR>
2019-10-24 23:30:34 +0200

    nieuwe user maken maakt ook account aan met random password

--- MultimedAPI/MultimedAPI/Controllers/UserController.cs
+++ MultimedAPI/MultimedAPI/Controllers/UserController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MultimedAPI.DTOs;
 using MultimedAPI.Models;
 using MultimedAPI.Models.IRepositories;
+
 
 namespace MultimedAPI.Controllers
 {
@@ -18,10 +20,13 @@ namespace MultimedAPI.Controllers
     {
 
         private readonly IUserRepository _userRepository;
-
-        public UserController(IUserRepository userRepository)
+        private readonly UserManager<IdentityUser> _userManager;
+        private static Random random = new Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0abcdefghijklmnopqrstuvw123456789";
+        public UserController(IUserRepository userRepository, UserManager<IdentityUser> userManager)
         {
             _userRepository = userRepository;
+            _userManager = userManager;
         }
 
         #region-- Http GET --
@@ -50,12 +55,17 @@ namespace MultimedAPI.Controllers
         #region -- POST --
 
         [HttpPost()]
-        public ActionResult<User> AddUser(UserDTO userDTO)
+        public async Task<ActionResult<User>> AddUserAsync(UserDTO userDTO)
         {
             User userToCreate = new User() { FirstName = userDTO.FirstName, FamilyName = userDTO.FamilyName, Email = userDTO.Email };
             _userRepository.AddUser(userToCreate);
+            var user = new IdentityUser { UserName = userDTO.Email, Email = userDTO.Email };
+            string password =  new string(Enumerable.Repeat(chars, 10)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+            await _userManager.CreateAsync(user, password);
             _userRepository.SaveChanges();
-            return CreatedAtAction(nameof(AddUser), new { id = userToCreate.UserId }, userToCreate);
+
+            return CreatedAtAction(nameof(AddUserAsync), new { id = userToCreate.UserId }, userToCreate);
         }
         #endregion
 
