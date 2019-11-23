0c7c50e71eb4d72275ae281eabf1225f3740f79e
NavaronBracke <navaron.bracke.y9090@student.hogent.be>
2019-11-23 17:57:05 +0100

    Changed AddChallenge to use DTO

---
+++ Projecten3_Backend/Projecten3_Backend/DTO/AddChallengeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projecten3_Backend.DTO
+{
+    public class AddChallengeDTO
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
